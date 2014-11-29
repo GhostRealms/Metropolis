10a7d1c61e911745bc21b5201c756924b52d38e3
River Marmorstein <river@rmarmorstein.com>
2014-11-29 18:25:27 -0500

    implement uuid tools class

---
+++ src/net/ghostrealms/metropolis/UUIDTools.java
@@ -0,0 +1,15 @@
+package net.ghostrealms.metropolis;
+
+/**
+ * Created by River on 28-Nov-14 23:56.
+ */
+public class UUIDTools {
+
+    /*
+    Class Provided to Convert, Manage, and Check UUIDs. Implementation for Alt Checks is also a feature that is planned
+    for the future.
+     */
+
+    private UUIDTools() {}
+
+}
