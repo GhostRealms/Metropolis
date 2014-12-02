495afff33128af93fa7c71322639916d7e0f83eb
River Marmorstein <river@rmarmorstein.com>
2014-12-01 23:48:50 -0500

    add some more methods to the Registrar, introduce the plot class

--- src/net/ghostrealms/metropolis/Registrar.java
+++ src/net/ghostrealms/metropolis/Registrar.java
@@ -1,6 +1,9 @@
 package net.ghostrealms.metropolis;
 
+import net.ghostrealms.exception.InvalidPlotException;
 import net.ghostrealms.protection.Plot;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
 import org.bukkit.World;
 
 import java.sql.*;
@@ -52,6 +55,70 @@ public class Registrar {
 
     public void registerNewResident(UUID id) {
 
+    }
+
+    /**
+     * Return a plot object from specified plot (x,y) coordinate pair
+     * @author River Marmorstein
+     * @param x Plot x coordinate
+     * @param y Plot y coordinate
+     * @return Plot object
+     */
+    public Plot getPlot(int x, int y) {
+
+        ResultSet results;
+
+        for(Plot plot : plotCache) {
+            if(plot.getX() == x && plot.getY() == y) {
+                return plot;
+            }
+        }
+
+        try {
+            Statement stmt = conn.createStatement();
+            results = stmt.executeQuery("SELECT * FROM " + Metro.getTableName(Metro.Tables.Plot) + " WHERE x = " + x +
+                                        " AND y = " + y + ";");
+
+            if(results.first() && results.getInt("x") == x && results.getInt("y") == y) {
+
+            } else {
+                Chunk chunk = main.getChunkAt(x,y);
+            }
+
+            stmt.close();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+
+        return null;
+    }
+
+    /**
+     * Return a plot object from the specified Bukkit location
+     * @author River Marmorstein
+     * @param location bukkit location object
+     * @return Plot object
+     */
+    public Plot getPlot(Location location) {
+        int x = location.getChunk().getX();
+        int y = location.getChunk().getZ();
+
+        return getPlot(x,y);
+    }
+
+    /**
+     *
+     * @param plot object to add to the cache
+     * @return
+     */
+    public boolean cachePlot(Plot plot) {
+        if(!(plotCache.contains(plot))) {
+            plotCache.add(plot);
+            return true;
+        }
+        return false;
     }
 
 
---
+++ src/net/ghostrealms/protection/PermissionType.java
@@ -0,0 +1,12 @@
+package net.ghostrealms.protection;
+
+/**
+ * Created by River on 30-Nov-14.
+ */
+public enum PermissionType {
+    EDIT,
+    USE,
+    ACCESS,
+    KILL,
+    OPEN;
+}
