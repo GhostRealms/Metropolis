96aa893e18b294d812d1d7e6114a380d0151efa3
River Marmorstein <river@rmarmorstein.com>
2015-01-11 18:13:51 -0500

    update main

--- src/main/java/net/ghostrealms/Metro.java
+++ src/main/java/net/ghostrealms/Metro.java
@@ -1,19 +1,37 @@
 package net.ghostrealms;
 
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Created by River on 1/11/2015.
+ * Metropolis Plugin Main
  */
 public class Metro extends JavaPlugin {
 
+    private Economy econ = null;
+
     @Override
     public void onEnable() {
+        if(!setupVault() || getServer().getPluginManager().getPlugin("Vault") == null) {
+            getLogger().severe("Could not instance vault");
+            this.getServer().getPluginManager().disablePlugin(this);
+        }
 
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public boolean setupVault() {
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
     }
 }
