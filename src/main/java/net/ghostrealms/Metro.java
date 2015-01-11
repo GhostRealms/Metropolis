36193a85dd228fac74ffbe094bbdc97b9c891856
River Marmorstein <river@rmarmorstein.com>
2015-01-11 18:20:01 -0500

    add the h2 connection in the Enable

--- src/main/java/net/ghostrealms/Metro.java
+++ src/main/java/net/ghostrealms/Metro.java
@@ -4,6 +4,10 @@ import net.milkbowl.vault.economy.Economy;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by River on 1/11/2015.
  * Metropolis Plugin Main
@@ -11,6 +15,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Metro extends JavaPlugin {
 
     private Economy econ = null;
+    private Connection connection = null;
 
     @Override
     public void onEnable() {
@@ -19,11 +24,29 @@ public class Metro extends JavaPlugin {
             this.getServer().getPluginManager().disablePlugin(this);
         }
 
+        try {
+            Class.forName("org.h2.Driver");
+            connection = DriverManager.getConnection("jdbc:h2:" + this.getDataFolder() + "metro");
+        } catch (ClassNotFoundException e) {
+            getLogger().severe("unable to instance h2");
+            e.printStackTrace();
+            getServer().getPluginManager().disablePlugin(this);
+        } catch (SQLException e) {
+            getLogger().severe("Unable to connect to the database");
+            e.printStackTrace();
+            getServer().getPluginManager().disablePlugin(this);
+        }
     }
 
     @Override
     public void onDisable() {
-
+        try {
+            connection.close();
+            connection = null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            //this should not happen.
+        }
     }
 
     public boolean setupVault() {
